ad747560dde4b02c064f9f34e7d88fa22f6637c0
oksyluka <oksana_l2@ukr.net>
2022-08-16 16:43:40 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,73 +1,38 @@
 import './css/styles.css';
-import fetchCountries from './fetchCountries';
-import debounce from 'lodash.debounce';
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
 
-const DEBOUNCE_DELAY = 300;
-const input = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
-function onInputChange() {
-  const isFilled = input.value.trim();
-  countryList.innerHTML = '';
-  countryInfo.innerHTML = '';
-  if (isFilled) {
-    fetchCountries(isFilled)
-      .then(inputEstimation)
-      .catch(error => {
-        if (error.code === '404') {
-          Notify.failure('Oops, there is no country with that name');
-          return;
-        }
-        Notify.failure('Unexpected error');
-      });
-  }
-}
 
-function inputEstimation(data) {
-  if (data.length > 10) {
-    Notify.info('Too many matches found. Please enter a more specific name.');
-    return;
-  } else if (data.length > 1 && data.length <= 10) {
-    createCountriesList(data);
-    return;
-  } else if (data.length === 1) {
-    createCountryInfo(data);
-  }
 
-  function createCountriesList(data) {
-    const markupCountries = data
-      .map(({ flags: { svg }, name: { official } }) => {
-        return `<li class="country-item"><img src="${svg}" alt="Flag of ${official}" width="100" height="50"/>
-        <p class="country-name">${official}</p></li>`;
-      })
-      .join('');
-    countryList.insertAdjacentHTML('afterbegin', markupCountries);
-  }
-}
 
-function createCountryInfo(data) {
-  const markupInfo = data.map(
-    ({
-      flags: { svg },
-      name: { official },
-      capital,
-      population,
-      languages,
-    }) => {
-      return `<div class="country-title"><img src="${svg}" alt="Flag of ${official}" width="100" height="50"/>
-        <p class="country">${official}</p></div>
-    <ul>
-      <li><span class="feature">Capital: </span>${capital}</li>
-      <li><span class="feature">Population: </span>${population}</li>
-      <li><span class="feature">Languages: </span>${Object.values(
-        languages
-      ).join(', ')}</li>
-      </ul>`;
-    }
-  );
-  countryInfo.insertAdjacentHTML('afterbegin', markupInfo);
-}
+<div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>
+
+
+const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
